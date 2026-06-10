9af78b82cd368ab5f097c184f31a0547ae248a2a
Manleen Kaur <manleen.kaur2005@gmail.com>
2026-06-10 12:47:59 +0530

    Fix API key and add working MVC architecture

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '9cb961e6-855c-4dcd-9144-642f31ac3928';
 export const MODAL_CLOSE_SEC = 2.5;
